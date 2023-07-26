6d897c73b79f9bcd01571a5e0763373bc6797397
patkoton <patkoton@yahoo.com>
2023-07-26 20:14:21 +0100

    Updated Lazy Loader

--- src/layout/Lazy.tsx
+++ src/layout/Lazy.tsx
@@ -3,7 +3,7 @@ import Logo from '../assets/images/blue_logo.png'
 const Lazy = () => {
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
-      <img alt='Lazy Loader Logo' src={Logo} className='w-80' />
+      <img alt='Lazy Loader Logo' src={Logo} className='w-20 md:w-40' />
     </div>
   )
 }
