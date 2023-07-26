d17dfd23b9485c67e38d79eb1fe3add6a66869e7
patkoton <patkoton@yahoo.com>
2023-07-26 20:09:56 +0100

    Added White Background Logo

--- src/layout/Lazy.tsx
+++ src/layout/Lazy.tsx
@@ -1,8 +1,9 @@
+import Logo from '../assets/images/blue_logo.png'
 
 const Lazy = () => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center font-roboto'>
-      <h4 className="font-bold text-2xl text-gray">PatPortfolio</h4>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <img alt='Lazy Loader Logo' src={Logo} className='w-80' />
     </div>
   )
 }
